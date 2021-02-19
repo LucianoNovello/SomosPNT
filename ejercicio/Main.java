ef0376cf4bfbe680b80c9a6a95070225ff935c49
Luciano Novello <56831260+LucianoNovello@users.noreply.github.com>
2021-02-19 12:48:17 -0300

    Update Main.java

--- ejercicio/Main.java
+++ ejercicio/Main.java
@@ -8,9 +8,7 @@ public class Main {
 		ListaProducto Lista = new ListaProducto();
 		Lista.AgregarProducto();
 		System.out.println(Lista.ListarProductos());
-		System.out.println("======================================");
-		System.out.println("Producto mas caro "+ Lista.PrecioMaximo());
-		System.out.println("Producto mas barato "+ Lista.PrecioMinimo());
+		
 		
 		
 		
